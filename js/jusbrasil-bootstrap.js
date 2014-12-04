2273e60f6a3547995431ebbae48fe075d5a15ba3
Shankar Cabus <shankar@jusbrasil.com.br>
2014-12-04 18:16:04 -0300

    Removing deprecated JS

--- js/jusbrasil-bootstrap.js
+++ js/jusbrasil-bootstrap.js
@@ -1,12 +1,3 @@
 window.onload = function(){
 
-  function changeBackground(e){
-    document.getElementsByClassName('colors')[0].style.background = window.getComputedStyle(e.target, null).backgroundColor;
-    return false;
-  }
-
-  var pallets = document.getElementsByClassName('colorPallet-color');
-  for (var i = 0; i < pallets.length; i++)
-    pallets[i].onclick = changeBackground;
-
 }
